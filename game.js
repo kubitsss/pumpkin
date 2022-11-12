604633abaf200292d7e2d561eb30e569cfa98ba0
kubits <99132845+kubitsss@users.noreply.github.com>
2022-11-12 05:15:27 +0000

    added pics and drawing functionality

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let config = {
 	type: Phaser.AUTO,
-    width: 800,
-    height: 500,
+    width: 1280,
+    height: 720,
     physics: {
         default: 'arcade',
         arcade: {
@@ -16,13 +16,25 @@ let config = {
 };
 const game = new Phaser.Game(config);
 function preload(){
-
+  this.load.image("pumpkin", "images/pumpkin.png");
+  this.load.image("knife", "images/knife.png");
+  this.load.image("eraser", "images/eraser.png");
+  this.load.image("dot", "images/dot.png")
 }
 
 function create(){
+  this.cameras.main.setBackgroundColor('#FF8C44');
+  this.add.sprite(640, 360, 'pumpkin');
+  this.add.sprite(200, 600, 'eraser');
+  this.add.sprite(1080, 600, 'knife');
 
+  this.input.on('pointermove', function (pointer) {
+    if (pointer.isDown) {
+      this.add.image(pointer.x, pointer.y, 'dot', Phaser.Math.Between(0, 5));
+    }
+  }, this);
 }
 
 function update(){
-
+  
 }
