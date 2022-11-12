3de482d28f8898d981c6998d957177f799821170
kubits <99132845+kubitsss@users.noreply.github.com>
2022-11-12 06:55:39 +0000

    drawing functionality is back but in vanilla javascript

--- game.js
+++ game.js
@@ -1,40 +1,35 @@
-let config = {
-	type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false
-        }
-    },
-    scene: {
-    	preload: preload,
-        create: create,
-        update: update
-    }
-};
-const game = new Phaser.Game(config);
-function preload(){
-  this.load.image("pumpkin", "images/pumpkin.png");
-  this.load.image("knife", "images/knife.png");
-  this.load.image("eraser", "images/eraser.png");
-  this.load.image("dot", "images/dot.png")
-}
+window.addEventListener("load", function() {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
 
-function create(){
-  this.cameras.main.setBackgroundColor('#FF8C44');
-  this.add.sprite(640, 360, 'pumpkin');
-  this.add.sprite(200, 600, 'eraser');
-  this.add.sprite(1080, 600, 'knife');
-
-  this.input.on('pointermove', function (pointer) {
-    if (pointer.isDown) {
-      this.add.image(pointer.x, pointer.y, 'dot', Phaser.Math.Between(0, 5));
-    }
-  }, this);
-}
-
-function update(){
+  //var
+  let painting = false;
+  var b = canvas.getBoundingClientRect();
+ 
   
-}
+  ctx.canvas.width = 1280;
+  ctx.canvas.height = 720;
+  function startPos(){
+    painting = true;
+  }
+
+  function finishedPos(){
+    painting = false;
+  }
+
+  function draw(e){
+    if(!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+
+
+    ctx.lineTo(e.x, e.y);
+    ctx.stroke();
+  } 
+
+  //eventListeners
+  canvas.addEventListener("mousedown", startPos);
+  canvas.addEventListener("mouseup", finishedPos);
+  canvas.addEventListener("mousemove", draw);
+  
+});
