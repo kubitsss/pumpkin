53c992a1b78254ac5994a92565e12ddba1edabfd
kubits <99132845+kubitsss@users.noreply.github.com>
2022-11-12 23:06:30 +0000

    ok i added canvasjs AND IT WORKS

--- game.js
+++ game.js
@@ -1,35 +1,59 @@
-window.addEventListener("load", function() {
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
+var canvas, stage;
+var drawingCanvas;
+var oldPt;
+var oldMidPt;
+var index;
 
-  //var
-  let painting = false;
-  var b = canvas.getBoundingClientRect();
- 
+function init() {
+	canvas = document.getElementById("canvas");
+	index = 0;
   
-  ctx.canvas.width = 1280;
-  ctx.canvas.height = 720;
-  function startPos(){
-    painting = true;
-  }
+	//check to see if we are running in a browser with touch support
+	stage = new createjs.Stage(canvas);
+	stage.autoClear = false;
+	stage.enableDOMEvents(true);
 
-  function finishedPos(){
-    painting = false;
-  }
+  canvas.width = 1280;
+  canvas.height = 720;
+	createjs.Touch.enable(stage);
+	createjs.Ticker.framerate = 24;
 
-  function draw(e){
-    if(!painting) return;
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
+	drawingCanvas = new createjs.Shape();
+
+	stage.addEventListener("stagemousedown", handleMouseDown);
+	stage.addEventListener("stagemouseup", handleMouseUp);
 
 
-    ctx.lineTo(e.x, e.y);
-    ctx.stroke();
-  } 
+	stage.addChild(drawingCanvas);
+	stage.update();
+}
 
-  //eventListeners
-  canvas.addEventListener("mousedown", startPos);
-  canvas.addEventListener("mouseup", finishedPos);
-  canvas.addEventListener("mousemove", draw);
+function handleMouseDown(event) {
+	if (!event.primary) { return; }
+	color = "#000000"
   
-});
+
+	oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+	oldMidPt = oldPt.clone();
+	stage.addEventListener("stagemousemove", handleMouseMove);
+}
+
+function handleMouseMove(event) {
+	if (!event.primary) { return; }
+	var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
+
+	drawingCanvas.graphics.clear().setStrokeStyle('15', 'round', 'round').beginStroke("black").moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+
+	oldPt.x = stage.mouseX;
+	oldPt.y = stage.mouseY;
+
+	oldMidPt.x = midPt.x;
+	oldMidPt.y = midPt.y;
+
+	stage.update();
+}
+
+function handleMouseUp(event) {
+	if (!event.primary) { return; }
+	stage.removeEventListener("stagemousemove", handleMouseMove);
+}
